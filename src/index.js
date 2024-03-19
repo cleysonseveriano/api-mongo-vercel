d43244bcde5c9fbe077e707572f990fc015485cf
cleysonseveriano <cleyson.tab@gmail.com>
2024-03-19 12:21:20 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
+
 require('dotenv').config();
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 const Produto = mongoose.model('Produto', { 
